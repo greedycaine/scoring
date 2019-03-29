remove calcWOE calcIV, fix calculation error on scoring stage, fix error on calculating bad/good distribution in bitable

build/lib/scoring/bivariate.py
@@ -2,20 +2,21 @@ import pandas as pd
 import numpy as np
 from .cleanning import delFromVardict
 
-def calcWOE(allGoodCnt, allBadCnt, eachGoodCnt, eachBadCnt):
+# # removed from version 0.0.8, replaced by calculating woe directly inside bitable
+# def calcWOE(allGoodCnt, allBadCnt, eachGoodCnt, eachBadCnt):
+#
+#     woe = np.log((eachGoodCnt / eachBadCnt) / (allGoodCnt / allBadCnt))
+#
+#     return woe
 
-    woe = np.log((eachGoodCnt / eachBadCnt) / (allGoodCnt / allBadCnt))
-
-    return woe
-
-
-def calcIV(allGoodCnt, allBadCnt, eachGoodCnt, eachBadCnt):
-    # calcIV(allGoodCnt, allBadCnt, eachGoodCnt, eachBadCnt, label='DEFAULT')
-    woe = calcWOE(allGoodCnt, allBadCnt, eachGoodCnt, eachBadCnt)
-    ivcolumn = (eachGoodCnt / allGoodCnt - eachBadCnt / allBadCnt) * woe
-    iv = sum(ivcolumn)
-
-    return ivcolumn, iv
+# # removed from version 0.0.8, replaced by calculating iv directly inside bitable
+# def calcIV(allGoodCnt, allBadCnt, eachGoodCnt, eachBadCnt):
+#     # calcIV(allGoodCnt, allBadCnt, eachGoodCnt, eachBadCnt, label='DEFAULT')
+#     woe = calcWOE(allGoodCnt, allBadCnt, eachGoodCnt, eachBadCnt)
+#     ivcolumn = (eachGoodCnt / allGoodCnt - eachBadCnt / allBadCnt) * woe
+#     iv = sum(ivcolumn)
+#
+#     return ivcolumn, iv
 
 
 def bivariate(df, col, label, withIV=True, missingvalue='missing', dealMissing=True):
@@ -30,8 +31,8 @@ def bivariate(df, col, label, withIV=True, missingvalue='missing', dealMissing=T
     bitable = pd.DataFrame({col: all[col], 'total': good + bad, 'good': good, 'bad': bad}). \
         replace(0, 0.001). \
         assign(totalDist=lambda x: x.total / sum(x.total),
-               goodDist=lambda x: x.bad / sum(x.bad),
-               badDist=lambda x: x.good / sum(x.good),
+               goodDist=lambda x: x.good / sum(x.good),
+               badDist=lambda x: x.bad / sum(x.bad),
                goodRate=lambda x: x.good / (x.total),
                badRate=lambda x: x.bad / (x.total)
                ). \

build/lib/scoring/scoring.py
@@ -14,16 +14,17 @@ def calcAB(odds=1/19, pdo=50, p0=600):
 #     B=pdo/ln(2)           (1)
 #     A=p0-Bln(odds)        (2)
     b=pdo/np.log(2)
-    a=p0-b*np.log(odds)
-    
+    a=p0+b*np.log(odds)
+
     return [a,b]
 
-def calcBinScore(df,bidict,b,coef):
-    n=0
-    
+
+def calcBinScore(df, bidict, b, coef):
+    n = 0
+
     for i in df.columns:
-        bidict[i]['score']=-b*coef[0][n]*bidict[i]['woe']
-        n=n+1
+        bidict[i]['score'] = round(-b * coef[0][n] * bidict[i]['woe'])
+        n = n + 1
     return bidict
 
 def mapScore(Xdf,Ydf,bidict,label):
@@ -39,12 +40,12 @@ def mapScore(Xdf,Ydf,bidict,label):
         
     return newdf
 
-def calcScore(df, model_intercept, a, b, label):
 
-    basescore=(a+b*model_intercept)[0]
-    df['score']=basescore+df.sum(axis=1)-df[label]
-    
-    return [df,basescore]
+def calcScore(df, model_intercept, a, b, label):
+    basescore = round((a - b * model_intercept)[0])
+    df['score'] = basescore + df.sum(axis=1) - df[label]
+
+    return [df, basescore]
 
 def scoring(Xdf,Ydf,label,m,bidict):
     